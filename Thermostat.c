55d4eb2c7799aac5d44826024a06c994f1cd4c5d
Off333 <36643550+Off333@users.noreply.github.com>
2024-10-22 00:44:51 +0200

    Update Thermostat.c

--- Thermostat.c
+++ Thermostat.c
@@ -9,46 +9,6 @@
 #include "potenciometer.h"
 #include "button_driver.h"
 
-
-/**
- * TODO: 
- * přidat state machine
- * 
- * wifi
- *  > připojení
- *  > ntp
- *  > jednoduchý web server (bude aktivní pořád?)
- *  > odpojování od wifi?
- * 
- * logika termostatu
- *  > rtc alarmy
- *  > nastavení různé teploty na různé časy
- *   > struktura pro ukládání
- *   > hlídání těchto časů přes alarmy
- *    > jak lze udělat křivky? -> například granuálně po 5 minutách měnit podle grafu křivky(funkce)
- * 
- * menu a ovládání
- *  > nastavení teplot
- *  > nastavitelná hysterze temploty 
- *  > základní nastavení
- *  > vypnutí/zapnutí webserveru?
- *  > úplné pozastavení monitorování teploty?
- * 
- * ukládání a statistika teplot
- * 
- * ---
- * krabička pro termostat
- *  > tlačítka a otočný úchyt pro potík
- *  > obrazovka
- *  > zapnutí/vypnutí termostatu?
- *  > otvory pro indikační ledky?
- * 
- * kalibrace teploměru (teploměr na pico)
- * 
- * dokumentace
- * 
- */
-
 /* main loop sleeping after finishing in ms */
 #define MAIN_LOOP_REFRESH_TIMER 200
 
