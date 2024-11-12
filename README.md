8c857284d1ff73f7e4a444711b088fe1e9feb35d
Off333 <0iO@email.cz>
2024-11-12 18:11:11 +0100

    added states structure lcd now showes time

--- README.md
+++ README.md
@@ -1,38 +1,50 @@
-/**
- * TODO: 
- * přidat state machine
- * 
- * wifi
- *  > připojení
- *  > ntp
- *  > jednoduchý web server (bude aktivní pořád?)
- *  > odpojování od wifi?
- * 
- * logika termostatu
- *  > rtc alarmy
- *  > nastavení různé teploty na různé časy
- *   > struktura pro ukládání
- *   > hlídání těchto časů přes alarmy
- *    > jak lze udělat křivky? -> například granuálně po 5 minutách měnit podle grafu křivky(funkce)
- * 
- * menu a ovládání
- *  > nastavení teplot
- *  > nastavitelná hysterze temploty 
- *  > základní nastavení
- *  > vypnutí/zapnutí webserveru?
- *  > úplné pozastavení monitorování teploty?
- * 
- * ukládání a statistika teplot
- * 
- * ---
- * krabička pro termostat
- *  > tlačítka a otočný úchyt pro potík
- *  > obrazovka
- *  > zapnutí/vypnutí termostatu?
- *  > otvory pro indikační ledky?
- * 
- * kalibrace teploměru (teploměr na pico)
- * 
- * dokumentace
- * 
- */
+**TODO**: 
+
+přidat state machine
+
+wifi
+ - připojení
+ - ntp
+ - jednoduchý web server (bude aktivní pořád?)
+ - odpojování od wifi?
+
+logika termostatu
+ - rtc alarmy
+ - nastavení různé teploty na různé časy
+    - struktura pro ukládání
+    - hlídání těchto časů přes alarmy
+        - jak lze udělat křivky? -> například granuálně po 5 minutách měnit podle grafu křivky(funkce)
+
+menu a ovládání
+ - nastavení teplot
+ - nastavitelná hysterze temploty 
+ - základní nastavení
+ - vypnutí/zapnutí webserveru?
+ - úplné pozastavení monitorování teploty?
+
+ukládání a statistika teplot
+
+krabička pro termostat
+ - tlačítka a otočný úchyt pro potík
+ - obrazovka
+ - zapnutí/vypnutí termostatu?
+ - otvory pro indikační ledky?
+
+kalibrace teploměru (teploměr na pico)
+
+vypnutí potenciometru když není potřeba
+
+dokumentace
+
+Relevantní odkazy:
+ - https://forums.raspberrypi.com/viewtopic.php?t=339289
+ - https://github.com/raspberrypi/pico-examples/tree/master/pico_w/wifi/tcp_server
+ - https://www.ti.com/lit/ds/symlink/lm35.pdf
+ - https://datasheets.raspberrypi.com/picow/pico-w-datasheet.pdf
+ - https://datasheets.raspberrypi.com/pico/raspberry-pi-pico-c-sdk.pdf
+ - https://github.com/raspberrypi/pico-examples/blob/master/pico_w/wifi/ntp_client/picow_ntp_client.c
+ - https://github.com/raspberrypi/pico-examples/tree/master/pico_w/wifi/httpd
+ - https://www.raspberrypi.com/documentation/pico-sdk/hardware.html
+ - https://www.instructables.com/Arduino-Thermostat/
+ - https://dzone.com/articles/how-to-build-your-own-arduino-thermostat
+ - https://moniteurdevices.com/knowledgebase/knowledgebase/what-is-the-difference-between-spst-spdt-and-dpdt/
--- Thermostat.c
+++ Thermostat.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "pico/stdlib.h"
 #include "hardware/rtc.h"
+#include "pico/util/datetime.h"
 
 #include "lcd.h"
 #include "analog_tmp_sensor.h"
@@ -10,7 +12,7 @@
 #include "button_driver.h"
 
 /* main loop sleeping after finishing in ms */
-#define MAIN_LOOP_REFRESH_TIMER 200
+#define MAIN_LOOP_REFRESH_TIMER 500
 
 
 /* temperature limits in °C */
@@ -25,6 +27,32 @@
 #define BTN_3_GPIO 17
 #define BTN_4_GPIO 16
 
+typedef enum {
+    S_SLEEP,
+    S_SHOW_TIME,
+    S_SHOW_TEMP,
+    S_SHOW_PROGS,
+    S_SHOW_SETN,
+    S_PROG_SEL,
+    S_PROG_TIME_SET,
+    S_PROG_WEEKDAY_SET,
+    S_PROG_TEMP_SET,
+    S_SETN_TIME,
+    S_SETN_HYST,
+    S_SETN_OFF
+} state_t;
+
+typedef enum {
+    NS_NONE,
+    NS_LEFT,
+    NS_RIGHT,
+    NS_ENTER,
+    NS_ESC
+} next_action_t;
+
+#define DEFAULT_STATE S_SHOW_TIME
+next_action_t next_action = NS_NONE;
+
 // void lcd_reset() {
 //     lcd_display_enable(true);
 //     lcd_clear();
@@ -36,7 +64,27 @@ void wake_up() {
     lcd_refresh_on_timer();
 }
 
-void lcd_show_temp_data(uint16_t temp, float temp_treshold) {
+// takhle se používá rtc:
+// char datetime_buf[256];
+// char *datetime_str = &datetime_buf[0];
+// rtc_get_datetime(&t);
+// datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+
+void show_time() {
+    static char datetime_buf[17];
+    static char *datetime_str = &datetime_buf[0];
+    static datetime_t dt;
+    static time_t t;
+    
+    rtc_get_datetime(&dt);
+    datetime_to_time(&dt, &t);
+    struct tm* time_struct = localtime(&t);
+    strftime(datetime_str, sizeof(datetime_buf), "%H:%M %d.%m.%y", time_struct);
+    lcd_write(datetime_str, LCD_LINE_1);
+    printf("showing time: %s\n", datetime_str);
+}
+
+void show_temp_data(uint16_t temp, float temp_treshold) {
     char temp_str[6];
     char write_str[17];
 
@@ -67,6 +115,67 @@ void check_relay_state(float current_temp, float set_temp) {
     } else if(current_temp < set_temp - TEMPERATURE_HYSTERSIS/2) {
         relay_on();
     }
+}
+
+// show_temp_data(get_temp_data(), get_temp_treshold());
+
+void main_loop_logic(state_t state) {
+    //pokud je zapnutý
+    // check_relay_state(convert_temp_to_float(get_temp_data()), get_temp_treshold());
+    
+
+    switch(state) {
+        case S_SHOW_TIME:
+            show_time();
+            break;
+        default:
+            break;
+    }
+}
+
+state_t main_loop_step() {
+
+    static state_t state = DEFAULT_STATE;
+
+    state_t next_state = state;
+
+    switch(state) {
+        case S_SHOW_TIME:
+            switch(next_action) {
+                case NS_LEFT:
+                    break;
+                case NS_RIGHT:
+                    break;
+                case NS_ENTER:
+                    break;
+                case NS_ESC:
+                    break;
+                case NS_NONE:
+                    break;
+            }
+            break;
+        default:
+            switch(next_action) {
+                case NS_LEFT:
+                    break;
+                case NS_RIGHT:
+                    break;
+                case NS_ENTER:
+                    break;
+                case NS_ESC:
+                    break;
+                case NS_NONE:
+                    break;
+            }
+            break;
+    }
+
+
+    if(state == next_state) {
+        //timer pro uspání MCU?
+    }
+    state = next_state;
+    return state;
 }
 
 int main() {
@@ -102,12 +211,6 @@ int main() {
     rtc_set_datetime(&t);
     sleep_us(64); // waiting for rtc to update
 
-    // takhle se používá rtc:
-    // char datetime_buf[256];
-    // char *datetime_str = &datetime_buf[0];
-    // rtc_get_datetime(&t);
-    // datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
-
     relay_init();
 
     if(lcd_init()) {
@@ -126,23 +229,28 @@ int main() {
     lcd_clear();
     printf("initialization done!");
 
+    state_t state = DEFAULT_STATE;
     while (true) {
-        check_relay_state(convert_temp_to_float(get_temp_data()), get_temp_treshold());
-
-        lcd_show_temp_data(get_temp_data(), get_temp_treshold());
-
-        //zobrazování na displeji
-        // if(display_timer > 0)  {
-        //     lcd_show_temp_data(temp, temp_treshold);
-        //     display_timer -= MAIN_LOOP_REFRESH_TIMER/1000;
-        // }
         
-        // if(display_timer == 0) {
-        //     lcd_display_enable(false);
-        // }
+        state = main_loop_step();
 
-        printf("main loop end\n");
+        main_loop_logic(state);
+
         sleep_ms(MAIN_LOOP_REFRESH_TIMER);
 
+        printf("main loop end, state: %i\n", state);
     }
 }
+
+
+//zobrazování na displeji
+// if(display_timer > 0)  {
+//     lcd_show_temp_data(temp, temp_treshold);
+//     display_timer -= MAIN_LOOP_REFRESH_TIMER/1000;
+// }
+
+// if(display_timer == 0) {
+//     lcd_display_enable(false);
+// }
+
+//printf("main loop end\n");
